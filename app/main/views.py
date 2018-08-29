f6f53d9e32db99b53e7e23270c247d6e27a8c747
Yansheng Liu <yansheng.liu2000@gmail.com>
2018-08-29 21:06:23 +1000

    add some htmls and main.views

--- app/main/views.py
+++ app/main/views.py
@@ -3,4 +3,13 @@ from flask import render_template
 
 @main.route('/',methods=['GET'])
 def home():
+    diary_dict={}
     return render_template("home.html")
+
+@main.route('/resume',methods=['GET'])
+def resume():
+    return render_template("resume.html")
+
+@main.route('/about_me', methods=['GET'])
+def about_me():
+    return render_template("about_me.html")
--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import app
 
 if __name__=="__main__":
-<<<<<<< HEAD
+    # app.run(host='0.0.0.0', port=5000)
     app.run()
-=======
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> 8af1e4387fe86ae288d85b14bd000b3369ed1fb4
